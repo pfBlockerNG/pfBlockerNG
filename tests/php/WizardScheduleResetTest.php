2b73861f301e1539b1e5ad0c525e67b903f674c8
Andre Brait <andrebrait@gmail.com>
2026-08-13 19:08:28 +0200

    fix: preserve scheduler master state

--- tests/php/WizardScheduleResetTest.php
+++ tests/php/WizardScheduleResetTest.php
@@ -39,22 +39,42 @@ final class WizardScheduleResetTest extends TestCase
 		}
 
 		$saved_config = $GLOBALS['config'] ?? NULL;
-		foreach (['', 'on'] as $master) {
-			$GLOBALS['config'] = ['installedpackages' => []];
-			$pfb_general_schedule = [
-				'pfb_scheduled_feed_updates' => $master,
-				'pfb_schedule_weekday' => '4',
-				'pfb_schedule_hour' => '5',
-				'pfb_schedule_minute' => '45',
-				'skipfeed' => '0',
-			];
-			eval(substr($source, $start, $end - $start));
+		$cases = [
+			'absent' => [NULL, 'on'],
+			'canonical off' => ['', ''],
+			'canonical on' => ['on', 'on'],
+			'legacy off' => ['off', ''],
+			'case variant' => ['OFF', ''],
+			'junk' => ['junk', ''],
+			'non-scalar' => [['on'], ''],
+		];
+		try {
+			foreach ($cases as $name => [$raw_master, $expected_master]) {
+				$GLOBALS['config'] = ['installedpackages' => []];
+				$pfb_general_schedule = [
+					'pfb_schedule_weekday' => '4',
+					'pfb_schedule_hour' => '5',
+					'pfb_schedule_minute' => '45',
+					'skipfeed' => '0',
+				];
+				if ($raw_master !== NULL) {
+					$pfb_general_schedule['pfb_scheduled_feed_updates'] = $raw_master;
+				}
+				eval(substr($source, $start, $end - $start));
 
-			$this->assertSame($pfb_general_schedule, array_intersect_key(
-				$new_config['pfblockerng']['config'][0],
-				$pfb_general_schedule
-			));
+				$this->assertSame([
+					'pfb_scheduled_feed_updates' => $expected_master,
+					'pfb_schedule_weekday' => '4',
+					'pfb_schedule_hour' => '5',
+					'pfb_schedule_minute' => '45',
+					'skipfeed' => '0',
+				], array_intersect_key(
+					$new_config['pfblockerng']['config'][0],
+					array_flip(['pfb_scheduled_feed_updates', 'pfb_schedule_weekday', 'pfb_schedule_hour', 'pfb_schedule_minute', 'skipfeed'])
+				), $name);
+			}
+		} finally {
+			$GLOBALS['config'] = $saved_config;
 		}
-		$GLOBALS['config'] = $saved_config;
 	}
 }
