40d9cd79ad21f899b2c5c7cbd51e6eb6e81ff773
Andre Brait <andrebrait@gmail.com>
2026-08-13 22:31:57 +0200

    test(ci): pin race failure contracts
    Assert caller argument forwarding and the exact TOP1M integrity failure reported by the regression.
    
    Refs #2324

--- tests/shell/run_in_docker_fallback_spec.sh
+++ tests/shell/run_in_docker_fallback_spec.sh
@@ -90,6 +90,7 @@ STUB_EOF
     When call wrapper true
     The status should be success
     The output should include 'DOCKER_RUN --rm --init'
+    The output should include 'ci-runner:8 true'
   End
 
   It 'runs in the container when the image is absent but pulls'
